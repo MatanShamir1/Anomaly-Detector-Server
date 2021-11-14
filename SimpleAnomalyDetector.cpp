56bb1169fe83ec29fec372bb4bc55965bce818b4
itamar <itamarb25@gmail.com>
2021-11-14 13:14:52 -0800

    adding documentation to minCircle and SimpleAnomalyDetector.

--- SimpleAnomalyDetector.cpp
+++ SimpleAnomalyDetector.cpp
@@ -1,6 +1,8 @@
 #include "SimpleAnomalyDetector.h"
 #include "anomaly_detection_util.h"
+
 const float oneDotOne = 1.2;
+
 SimpleAnomalyDetector::SimpleAnomalyDetector() {
 
 }
@@ -15,6 +17,7 @@ void SimpleAnomalyDetector::learnNormal(const TimeSeries &ts) {
     int num_of_samples = ts.getNumSamples();
     vector<float> f1, f2;
     int numFeatures = ts.getNumFeatures();
+    // go over every feature and find the most correlated feature of him.
     for (int i = 0; i < numFeatures; i++) {
         //if no matching correlation, this will remain -1.
         int currentCorrelate = -1;
@@ -22,38 +25,44 @@ void SimpleAnomalyDetector::learnNormal(const TimeSeries &ts) {
         f1 = ts.getDataOfFeature(i);
         for (int j = i + 1; j < numFeatures; j++) {
             f2 = ts.getDataOfFeature(j);
-            if (abs(pearson(&f1[0],&f2[0], ts.getNumSamples())) > correlation) {
+            // ask if the correlation is bigger from the current correlation.
+            if (abs(pearson(&f1[0], &f2[0], ts.getNumSamples())) > correlation) {
                 currentCorrelate = j;
                 correlation = pearson(&f1[0], &f2[0], num_of_samples);
             }
         }
+        // now after go over all the feature and if there is someone that correlate , add him to the vector of the
+        // correlated feature.
         if (currentCorrelate != -1) {
             correlatedFeatures appendData;
             appendData.corrlation = correlation;
             appendData.feature1 = ts.getNameOfFeature(i);
             appendData.feature2 = ts.getNameOfFeature(currentCorrelate);
-            vector<Point*> featurePoint = createPoints(f1, ts.getDataOfFeature(currentCorrelate), num_of_samples);
+            // create point from the two feature.
+            vector<Point *> featurePoint = createPoints(f1, ts.getDataOfFeature(currentCorrelate), num_of_samples);
             appendData.lin_reg = linear_reg(&(featurePoint[0]), num_of_samples);
-            appendData.threshold = maxDevPoint(appendData.lin_reg, &(featurePoint[0]), num_of_samples)*oneDotOne;
+            // send to check the most further point from the linear.
+            appendData.threshold = maxDevPoint(appendData.lin_reg, &(featurePoint[0]), num_of_samples) * oneDotOne;
             this->cf.push_back(appendData);
-            // delete points
+            // delete  points that we created on the heap.
             int length = ts.getNumSamples();
-            for(size_t k=0;k<length;k++)
+            for (size_t k = 0; k < length; k++)
                 delete featurePoint[k];
         }
     }
 }
-//correct for sure
-vector<Point*> SimpleAnomalyDetector::createPoints(const vector<float>& f1, const vector<float>& f2, int size) {
-    vector<Point*> featuresPoints;
+
+// the function create point with the two feature.
+vector<Point*> SimpleAnomalyDetector::createPoints(const vector<float> &f1, const vector<float> &f2, int size) {
+    vector<Point *> featuresPoints;
     for (int i = 0; i < size; i++) {
-        auto* p = new Point(f1[i], f2[i]);
+        auto *p = new Point(f1[i], f2[i]);
         featuresPoints.push_back(p);
     }
     return featuresPoints;
 }
-
-float SimpleAnomalyDetector::maxDevPoint(Line f1,Point** points, int size) {
+// the function check the most distance point from the linear.
+float SimpleAnomalyDetector::maxDevPoint(Line f1, Point **points, int size) {
     float maxDev = 0;
     for (int i = 0; i < size; i++) {
         float check = dev(*(points[i]), f1);
@@ -67,14 +76,14 @@ float SimpleAnomalyDetector::maxDevPoint(Line f1,Point** points, int size) {
 vector<AnomalyReport> SimpleAnomalyDetector::detect(const TimeSeries &ts) {
     vector<AnomalyReport> report;
     //each variable initiated in the for loop is a const reference, not a shallow copy!
-    for (const correlatedFeatures& i1: this->cf) {
+    for (const correlatedFeatures &i1: this->cf) {
         //we use create points because this is data of a new input of a flight.
-        vector<Point*> dataPoints = createPoints(ts.getDataOfFeature(ts.indexOfFeature(i1.feature1)),
-                                                 ts.getDataOfFeature(ts.indexOfFeature(i1.feature2)),
-                                                 ts.getNumSamples());
-        for(size_t i=0;i<dataPoints.size();i++){
+        vector<Point *> dataPoints = createPoints(ts.getDataOfFeature(ts.indexOfFeature(i1.feature1)),
+                                                  ts.getDataOfFeature(ts.indexOfFeature(i1.feature2)),
+                                                  ts.getNumSamples());
+        for (size_t i = 0; i < dataPoints.size(); i++) {
             if (dev(*dataPoints[i], i1.lin_reg) > i1.threshold) {
-                AnomalyReport deviation = AnomalyReport(i1.feature1 + "-" + i1.feature2, i+1);
+                AnomalyReport deviation = AnomalyReport(i1.feature1 + "-" + i1.feature2, i + 1);
                 report.push_back(deviation);
             }
         }
--- minCircle.h
+++ minCircle.h
@@ -27,15 +27,19 @@ Circle findMinCircle(Point **points, size_t size);
 // you can add here additional methods
 Circle helpToFind(vector<Point> &, vector<Point>, int);
 
-Circle min_circle_trivial(vector<Point> &);
+Circle createCircle(vector<Point> &);
 
-Circle circle_from(const Point &, const Point &);
+Circle createCircleFrom2Points(const Point &, const Point &);
 
-bool is_valid_circle(const Circle &, const vector<Point> &);
-Point get_circle_center(double bx, double by,double cx, double cy);
-bool is_inside(const Circle& c, const Point& p);
-float dist(const Point& a, const Point& b);
-Circle circle_from(const Point &, const Point &, const Point &);
+bool isEnclosingCircle(const Circle &, const vector<Point> &);
+
+Point findCenter(double, double, double, double);
+
+bool isInside(const Circle &, const Point &);
+
+float distance(const Point &, const Point &);
+
+Circle createCircleFrom3Points(const Point &, const Point &, const Point &);
 
 
 #endif /* MINCIRCLE_H_ */
