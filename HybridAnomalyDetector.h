e88acb8e1e9f26977dca087276fa597de31e7a0d
itamar <itamarb25@gmail.com>
2021-12-09 23:22:38 -0800

    adding HybridAnomalyDetector.cpp abd HybridAnomalyDetector.h files

---
+++ HybridAnomalyDetector.h
@@ -0,0 +1,15 @@
+
+#ifndef HYBRIDANOMALYDETECTOR_H_
+#define HYBRIDANOMALYDETECTOR_H_
+
+#include "SimpleAnomalyDetector.h"
+#include "minCircle.h"
+
+class HybridAnomalyDetector:public SimpleAnomalyDetector {
+public:
+    HybridAnomalyDetector();
+    virtual ~HybridAnomalyDetector();
+
+};
+
+#endif /* HYBRIDANOMALYDETECTOR_H_ */
